12faef9a370d9f2a650e48b2324576ddb0ef6c90
Davidojekunle <davidoiekunle@gmail.com>
2024-08-20 21:39:33 +0100

    added cross

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.auth import auth
 from routes.users import users_router
 from routes.admin import admin_route
 from routes.payment import subs_route
+
 app = FastAPI()
 
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
